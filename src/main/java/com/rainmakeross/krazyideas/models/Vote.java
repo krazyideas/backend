462f60a3b3b27b17eae97c005f6c498f44831d8f
Hongkai Liu <hongkailiu@users.noreply.github.com>
2016-06-17 09:52:18 -0400

    add search for vote by key

--- src/main/java/com/rainmakeross/krazyideas/models/Vote.java
+++ src/main/java/com/rainmakeross/krazyideas/models/Vote.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
@@ -17,11 +17,11 @@ public class Vote {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "personId")
     private Person person;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name="ideaId")
     private Idea idea;
 
